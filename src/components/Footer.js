517555592dd778599a798c50e9a22388503fab4b
Ruby Reed <rubysreed@gmail.com>
2022-03-25 11:54:06 -0600

    footer and contact responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import map from "../images/map.png"
 import facebook from "../images/facebook.png"
 import phone from "../images/phone.png"
 import email from "../images/email.png"
-// import marker from "../images/marker.png"
+import marker from "../images/marker.png"
 import { useNavigate } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
@@ -14,44 +14,45 @@ const Footer = () => {
         navigate(eventKey);
     };
     return (
-    <div fixed="bottom" style={{backgroundColor:"#000000", width:"100%",  bottom:"0", boxShadow: "rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset"}}>
-        <div style={{display:"flex", justifyContent:"space-between"}}>
-        <div style={{paddingTop:"2rem",paddingRight:"2rem",paddingLeft:"2rem",color:"white",width: "25%",display:"flex", flexDirection:"column", alignItems:"center", borderRight:"3px solid white"}}>
+    <div className="footer" fixed="bottom" style={{minWidth:"550px"}}>
+        <div className="footerbody">
+        <div className="footerbio">
             <h4 style={{marginBottom:"1rem"}}>DAILY FIRE PROTECTION</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Lobortis 
                  mattis aliquam faucibus purus in. </p>
         </div>
-        <div style={{padding:"2rem",width:"25%", display:"flex", flexDirection:"column", alignItems:"center", borderRight:"3px solid white"}}> 
-            <h4 style={{color:"white", marginBottom:"1rem"}}>LOCATION</h4>
-            <img style={{height:"10rem"}} src={map}/>
-            {/* <img style={{height:"2rem", width:"2rem", margin:".5rem"}} src={marker}/> */}
-            <p style={{color:"white", marginTop:".5rem"}}>Add office address here</p>
-        </div>
-        <Nav onSelect={handleSelect} style={{width:"25%",display:"flex", flexDirection:"column", alignItems:"center", padding:"2rem", borderRight:"3px solid white"}}>
-            <h4 style={{color:"white", marginBottom:"1rem"}}>NAVIGATE</h4>
-            <Nav.Link className="footerlink" eventKey="/" style={{padding:".7rem"}}>HOME</Nav.Link>
-            <Nav.Link className="footerlink" eventKey="/about" style={{padding:".7rem"}}>ABOUT</Nav.Link>
-            <Nav.Link className="footerlink" eventKey="/services" style={{padding:".7rem"}}>SERVICES</Nav.Link>
-            <Nav.Link className="footerlink" eventKey="/contact" style={{padding:".7rem"}}>CONTACT</Nav.Link>
+        <div className="footernavdiv">
+            <h4 className="footernavtitle">NAVIGATE</h4>
+        <Nav className="footernav" onSelect={handleSelect}>
+            <Nav.Link className="footerlink" eventKey="/" >HOME</Nav.Link>
+            <Nav.Link className="footerlink" eventKey="/about" >ABOUT</Nav.Link>
+            <Nav.Link className="footerlink" eventKey="/services" >SERVICES</Nav.Link>
+            <Nav.Link className="footerlink" eventKey="/contact" >CONTACT</Nav.Link>
         </Nav>
-        <div style={{width:"25%",color:"white", padding:"2rem"}}>
+        </div>
+        <div className="footerinfo">
             <h4 style={{marginBottom:"1.5rem"}}>GET IN TOUCH</h4>
         <div style={{display:"flex"}}>
-            <img style={{height:"2rem"}} src={facebook}/> &nbsp; &nbsp;
+            <img className="footerimg" src={facebook}/> &nbsp; &nbsp;
             <p>Follow us on Facebook</p>
+            {/* add in link to facebook */}
         </div>
         <div style={{display:"flex"}}>
-            <img style={{height:"2rem"}} src={phone}/> &nbsp; &nbsp;
+            <img className="footerimg" src={phone}/> &nbsp; &nbsp;
             <p>(208) 899-9837</p>
         </div>
         <div style={{display:"flex"}}>
-            <img style={{height:"2rem"}} src={email}/> &nbsp; &nbsp;
+            <img className="footerimg" src={email}/> &nbsp; &nbsp;
             <p>admin@dailyfireprotection-nw.com</p>
         </div>
+        <div style={{display:"flex"}}>
+            <img className="footerimg" src={marker}/> &nbsp; &nbsp;
+            <p>Meridian, ID</p>
         </div>
         </div>
-        <div style={{width: "100%", backgroundColor:"#757575",color:"white", display:"flex", justifyContent:"center", alignContent:"center"}}>
+        </div>
+        <div className="footercopyright">
             <p style={{margin:"0rem", padding:".6rem"}}>Ⓒ 2022 by Ruby Reed</p>
         </div>
     </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,87 @@
-import React from "react";
+import React, {useState} from "react";
+import { Button, Container, Form } from "react-bootstrap";
 
 const Contact = () => {
+    const [submitted, setSubmitted] = useState(false);
+    const handleSubmit = () => {
+      setTimeout(() => {
+        setSubmitted(true);
+      }, 100);
+    };
+  
+    if (submitted) {
+      return (
+        <Container style={{ minWidth:"12rem", marginLeft: "15vw", marginRight: "15vw",marginTop: "10vw", borderRadius:".3rem",padding:"4rem",display:"flex",textAlign:"center",flexDirection:"column"}}>
+          <h1 style={{color:"white"}}>Thank you!</h1>
+          <p style={{color:"white"}}>We'll be in touch soon.</p>
+        </Container>
+      );
+    }
     return (
-    <div>
-        <h1>Contact</h1>
-    </div>
-    )}
-
+        <div style={{display:"flex", justifyContent:"center", minWidth:"550px"}}>
+        <Container style={{marginBottom:"5rem", margin:"2rem"}} className="contactcontainer">
+            <h1 style={{color:"white"}} className="contactheader">
+                Contact Us
+            </h1>
+            <form
+            // action={FORM_ENDPOINT}
+            onSubmit={handleSubmit}
+            method="POST"
+            target="_blank"
+            >
+    
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <label style={{color:"white"}}>
+                Name:
+              </label>
+              <br/>
+              <Form.Control 
+                type="text"
+                placeholder="Name*"
+                name="name"
+                required
+                />
+            </Form.Group>
+    
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <label style={{color:"white"}}>
+                  Email:
+                </label>
+                <br/>
+                <Form.Control 
+                type="email" 
+                placeholder="Email*"
+                required/>
+          </Form.Group>
+    
+          <Form.Group className="mb-3" controlId="formBasicSubject">
+                <label style={{color:"white"}}>
+                  Subject:
+                </label>
+                <br/>
+                <Form.Control 
+                type="subject" 
+                placeholder="Subject*"
+                required/>
+          </Form.Group>
+    
+          <Form.Group className="mb-3" controlID="formTextArea">
+            <label style={{color:"white"}}>Message:</label>
+            <br/>
+            <Form.Control placeholder="Message*" as="textarea" rows={5} required/>
+          </Form.Group>
+    
+          <Form.Group>
+            <Button
+            type="submit"
+            style={{backgroundColor:"black", border:"none"}}
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </form>
+    </Container>
+          </div>
+        )
+    }
 export default Contact;
