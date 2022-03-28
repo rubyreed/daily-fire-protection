d9951653bd018643a07d4a7cb262e2536f6c2e35
Ruby Reed <rubysreed@gmail.com>
2022-03-27 21:47:11 -0600

    services done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Contact from './pages/Contact';
-import Action from './pages/Action';
+// import Action from './pages/Action';
 import Services from './pages/Services';
 import About from './pages/About';
 
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/about" element={<About/>}/>
-          <Route path="/action" element={<Action/>}/>
+          {/* <Route path="/action" element={<Action/>}/> */}
           <Route path="/services" element={<Services/>}/>
           <Route path="/contact" element={<Contact/>}/>
       </Routes>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,54 @@
 import React from "react";
+import pencil from "../images/pencil.png"
+import toolbox from "../images/toolbox.gif"
+import clipboard from "../images/clipboard.png"
 
 const Services = () => {
     return (
     <div>
-        <h1>Services</h1>
+        <h1 className="servicetitle">Services</h1>
+        <div className="services">
+            <div className="service serviceborder">
+                <img className="serviceicon" src={pencil}/>
+                <h2 className="servicehead">Design</h2>
+                <p className="servicep">We work with engineers following strict standards for fire suppression,
+                safety, and damage control in each facility.
+                </p>
+            </div>
+            <div className="service serviceborder">
+                <img className="serviceicon" src={toolbox}/>
+                <h2 className="servicehead">Installation</h2>
+                <p className="servicep">We offer full installation, providing our clients with expert service while 
+                    meeting specific code requirements.
+                </p>
+            </div>
+            <div className="service">
+                <img className="serviceicon" src={clipboard}/>
+                <h2 className="servicehead">Inspection, Repair & Service</h2>
+                <p className="servicep">In addition to design and installation we provide maintenance, evaluation, routine
+                    inspection, testing, monitoring, repair and replacement.
+                </p>
+            </div>
+        </div>
+        <div className="protect">
+            <div className="protecttext">
+            <h2 className="servicehead" style={{paddingBottom:"1.5vw"}}>Let us Protect your Assets!</h2>
+            <p className="servicep">Daily Fire Protection-NW is a licensed and insured company available to meet your needs in Fire 
+                Protection, From design to installation, we are here to provide service from start to finish.
+            </p>
+            </div>
+            <ul className="protectlist">
+                <li>Installation</li>
+                <li>Fire Suppression</li>
+                <li>Sprinklers</li>
+                <li>Inspections</li>
+                <li>Fire Alarm Systems</li>
+                <li>Alarm Monitoring</li>
+                <li>Service and Repair</li>
+                <li>Corrosion Management</li>
+            </ul>
+        </div>
+        <h1 className="servicetitle">See Us in Action</h1>
     </div>
     )}
 
