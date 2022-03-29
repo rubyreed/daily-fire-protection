abc9f0360512f06fd48888ada9fc44d8c09badd6
Ruby Reed <rubysreed@gmail.com>
2022-03-28 21:37:36 -0600

    home started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
-import logo from "../images/DFP_logo_cutout.png";
+import logo from "../images/DFP_logo_cutout.png"
+import blueprintcopy from "../images/blueprint copy.png"
+import blueprint2 from "../images/blueprint2.png"
 
 const Home = () => {
     return (
-    <div style={{display:"flex", justifyContent:"center"}}>
-        <h1 style={{color:"white"}}>DAILY FIRE PROTECTION</h1>
-        <h2 style={{color:"white"}}>Protecting Property & Saving Lives</h2>
+    <div style={{position:"relative", textAlign:"center", color:"white"}}>
+        <img className="homeimg" src={blueprint2}/>
+        <div className="headerdiv">
+        <h1 style={{fontSize:"max(8rem)", fontWeight:"900"}}>DAILY FIRE PROTECTION</h1>
+        <h2 style={{fontSize:"max(4rem)"}}>Protecting Property & Saving Lives</h2>
+        </div>
+        {/* <div className="headerdiv">
+        <h1 className="homehead">DAILY FIRE PROTECTION</h1>
+        <h2 className="hometitle">Protecting Property & Saving Lives</h2>
+        </div> */}
     </div>
     )}
 
