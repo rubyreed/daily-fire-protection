8626c74ff9a67c05a793827bb6c94b90ad59bb56
Ruby Reed <rubysreed@gmail.com>
2022-03-31 14:32:14 -0600

    about adjusted with photos

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,17 @@
 import React from "react";
 import person from "../images/person.png"
 import people from "../images/people.jpg"
+import family from "../images/family.png"
 
 const About = () => {
     return (
-        <div className="about">
+        <div className="about" style={{minWidth:"554px"}}>
             <h1 style={{marginBottom:"3rem"}} className="title">About Us</h1>
         <div className="facts">
-            <h2 className="fact">Est. 2019</h2>
-            <h2 className="fact">Ada County, Canyon County & Surrounding Areas</h2>
-            <h2 className="fact">24 Hour Service</h2>
-            <h2 className="fact">Family-Oriented, Ethical Business Practices</h2>
+            <h2 className="fact1">Est. 2019</h2>
+            <h2 className="fact2">Ada County, Canyon County & Surrounding Areas</h2>
+            <h2 className="fact3">24 Hour Service</h2>
+            <h2 className="fact4">Family-Oriented, Ethical Business Practices</h2>
         </div>
         <div className="bio">
             <p style={{margin:0,textAlign:"center"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
@@ -25,11 +26,12 @@ const About = () => {
         </div>
         <h1 className="title">Meet the Team</h1>
         <div style={{display:"flex", justifyContent:"center"}}>
-        <img style={{height:"30vw", borderRadius:".5rem"}} src={people}/>
+        <img style={{height:"35vw", borderRadius:".5rem"}} src={people}/>
         </div>
         <div className="aboutsection">
             <div  className="personbiodiv">
-            <h2 style={{fontWeight:"900", marginBottom:"2rem"}}>Chris</h2>
+            <h2>Chris Daily</h2>
+            <img className="personimgsmall"src={family}/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim 
                 suspendisse in est ante in. Vel turpis nunc eget lorem dolor sed viverra ipsum. 
@@ -37,12 +39,13 @@ const About = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua.</p>
             </div>
-            <img className="personimg"src={people}/>
+            <img className="personimg"src={family}/>
         </div>
-        <div style={{marginBottom:"5rem"}} className="aboutsection">
-            <img className="personimg" src={people}/>
+        <div style={{marginBottom:"3rem"}} className="aboutsection">
+            <img className="personimg" src={family}/>
             <div className="personbiodiv">
-            <h2 style={{fontWeight:"900", marginBottom:"2rem"}}>Paul</h2>
+            <h2 className="abouttitle">Paul Vaughn</h2>
+            <img className="personimgsmall" src={family}/>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua. Risus pretium quam vulputate dignissim 
                 suspendisse in est ante in. Vel turpis nunc eget lorem dolor sed viverra ipsum. 
