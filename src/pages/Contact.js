3a6f99cc533febfa051bc2a6c4a1e5d3cdcc42c7
Ruby Reed <rubysreed@gmail.com>
2022-03-31 16:35:30 -0600

    added contact bar

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,8 @@
 import React, {useState} from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import phone from "../images/phone.png"
+import email from "../images/email.png"
+import facebook from "../images/facebook.png"
 
 const Contact = () => {
     const [submitted, setSubmitted] = useState(false);
@@ -19,17 +22,32 @@ const Contact = () => {
     }
     return (
         <div style={{display:"flex", justifyContent:"center", minWidth:"550px"}}>
-        <Container style={{ marginRight:"2rem", marginLeft:"2rem", marginBottom:"3rem"}} className="contactcontainer">
+        <Container style={{ marginRight:"2rem", marginLeft:"2rem", marginBottom:"3rem"}}>
             <h1 className="servicetitle">
-                Contact Us
+                Get in Touch
             </h1>
+            <div className="contactdiv" style={{marginTop:"2rem"}}>
+              <div className="connect connectborder">
+                <img className="connectimg" src={phone}/>
+                <h4 className="connecttext">(208) 899-9837</h4>
+              </div>
+              <div className="connect connectborder">
+                <img className="connectimg" src={email}/>
+                <h4 className="connecttext">admin@dailyfireprotection-nw.com</h4>
+                </div>
+              <div className="connect">
+                <img className="connectimg" src={facebook}/>
+                <h4 className="connecttext">Connect with us on Facebook</h4>
+                </div>
+            </div>
             <form
             // action={FORM_ENDPOINT}
             onSubmit={handleSubmit}
             method="POST"
             target="_blank"
+            style={{backgroundColor:"#424242", padding:"2rem", borderRadius:".5rem", marginTop:"2rem"}}
             >
-    
+              <h1 className="message">Send us a Message</h1>
             <Form.Group className="mb-3" controlId="formBasicName">
               <label style={{color:"white"}}>
                 Name:
